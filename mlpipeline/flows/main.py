954c0b9b3ee0eeee63a53480328a9b4f2fd8bb0b
Aadidev Sooknanan <a.aadi380@gmail.com>
2024-10-25 13:11:02 -0400

    update prefect parameters for table-paths

--- mlpipeline/flows/main.py
+++ mlpipeline/flows/main.py
@@ -3,11 +3,11 @@ from typing import Dict, Optional
 
 from prefect import flow
 
-from mlpipeline.feature_engineering import feature_engineering_flow
-
 # from ..models.evaluate import evaluate_model
 # from ..models.train import train_model
 from mlpipeline.utils.config import Config, load_config
+
+from .feature_engineering import feature_engineering_flow
 
 logger = logging.getLogger(__name__)
 
